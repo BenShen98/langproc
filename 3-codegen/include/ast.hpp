996b09ccb081165c65a8c28ec50e1144314e4f49
leyang shen <shenleyang007@gmail.com>
2019-02-14 17:38:26 +0000

    add logic except branch

--- 3-codegen/include/ast.hpp
+++ 3-codegen/include/ast.hpp
@@ -40,66 +40,66 @@ struct Tree
 // by spec, the output of program will be on STDCOUT
 
 // Establishes a label (jump target)
-inline void label(std::string& str){
+inline void label(const std::string& str){
   std::cout<<":"<<str<<" "<<std::flush;//space or change line?
 }
 
 // Loads the decimal immediate value into the destination register
-inline void constant(std::string& reg, std::string& imm){
+inline void constant(const std::string& reg, const std::string& imm){
   std::cout<<"const "<<reg<<' '<<imm<<'\n'<<std::flush;
 }
-inline void constant(std::string& reg, int imm){
+inline void constant(const std::string& reg, int imm){
   std::string imm_str=std::to_string(imm);
   constant(reg,imm_str);
 }
 
 // Read a value from input, and put it into the destination register.
-inline void input(std::string& reg){
+inline void input(const std::string& reg){
   std::cout<<"input "<<reg<<'\n'<<std::flush;
 }
 
 // Place the input parameter with index immVal into destination register.
-inline void param(std::string& reg, std::string& imm){
+inline void param(const std::string& reg, const std::string& imm){
   std::cout<<"param "<<reg<<' '<<imm<<'\n'<<std::flush;
 }
-inline void param(std::string& reg, int imm){
+inline void param(const std::string& reg, int imm){
   std::string imm_str=std::to_string(imm);
   param(reg,imm_str);
 }
 
 
 // Write the value in source register into the output stream.
-inline void output(std::string& reg){
+inline void output(const std::string& reg){
   std::cout<<"output "<<reg<<'\n'<<std::flush;
 }
 
 // Add the two source registers and write to destination.
-inline void add(std::string& regDst, std::string& regA, std::string& regB){
+inline void add(const std::string& regDst, const std::string& regA, const std::string& regB){
   std::cout<<"add "<<regDst<<' '<<regA<<' '<<regB<<'\n'<<std::flush;
 }
 
 // Subtract the two source registers and write to destination.
-inline void sub(std::string& regDst, std::string& regA, std::string& regB){
+inline void sub(const std::string& regDst, const std::string& regA, const std::string& regB){
   std::cout<<"sub "<<regDst<<' '<<regA<<' '<<regB<<'\n'<<std::flush;
 }
 
 // If regA < regB, then regDst=1, otherwise regDst=0.
-inline void lt(std::string& regDst, std::string& regA, std::string& regB){
+inline void lt(const std::string& regDst, const std::string& regA, const std::string& regB){
   std::cout<<"lt "<<regDst<<' '<<regA<<' '<<regB<<'\n'<<std::flush;
 }
 
 // If regA == regB, then jump to label.
-inline void beq(std::string& regA, std::string& regB, std::string& label){
+inline void beq(const std::string& regA, const std::string& regB, const std::string& label){
   std::cout<<"beq "<<regA<<' '<<regB<<" :"<<label<<'\n'<<std::flush;
 }
 
 // If regA != regB, then jump to label.
-inline void bne(std::string& regA, std::string& regB, std::string& label){
+inline void bne(const std::string& regA, const std::string& regB, const std::string& label){
   std::cout<<"bne "<<regA<<' '<<regB<<" :"<<label<<'\n'<<std::flush;
 }
 
 // Halt the program and return value in srcReg.
-inline void halt(std::string& Reg){
+inline void halt(const std::string& Reg){
   std::cout<<"halt "<<Reg<<'\n'<<std::flush;
 }
 
--- 3-codegen/src/ast_compile.cpp
+++ 3-codegen/src/ast_compile.cpp
@@ -4,11 +4,26 @@
 #include <regex>
 
 static int makeNameUnq=0;
+const static std::string zeroReg="zero";
+
+
+//all register are one time use, excpet the variable register
+
+//variable in this code exist as a register
+//when read variable, a copy of variable register is returned to the destReg (i.e. snipshot)
+//when modify the variable, the variable register need to be the destReg, the update will be reflected on the future read
+
+
+inline void copyReg(const std::string& regDst, const std::string& reg){
+  //copy register value by add 0
+  add(regDst,reg,zeroReg);
+}
 
 static std::string makeReg()
 {
     return "reg_"+std::to_string(makeNameUnq++);
 }
+
 
 static std::string makeLabel()
 {
@@ -23,20 +38,22 @@ void CompileRec(
     std::regex reId("^[a-z][a-z0-9]*$");
 
     if( regex_match( program->type, reNum ) ){
-        std::cout<<"const "<<destReg<<" "<<program->type<<std::endl;
+        constant(destReg,program->type);
+        // std::cout<<"const "<<destReg<<" "<<program->type<<std::endl;
 
     }else if( regex_match( program->type, reId ) ){
-        std::string constReg = makeReg();
-        constant(constReg,program->type);
-
+        //a snipshot of variable is created, passed as the return value
+        copyReg(destReg,program->type);
 
     }else if(program->type=="Input"){
-
+      input(destReg);
 
     }else if(program->type=="Param"){
       param(destReg,program->value);
 
     }else if(program->type=="Output"){
+      CompileRec(destReg,program->branches.at(0));
+      output(destReg);
 
     }else if(program->type=="LessThan"){
 
@@ -45,10 +62,20 @@ void CompileRec(
       CompileRec(regA, program->branches.at(0));
       std::string regB=makeReg();
       CompileRec(regB, program->branches.at(1));
+      add(destReg,regA,regB);
 
     }else if(program->type=="Sub"){
+      std::string regA=makeReg();
+      CompileRec(regA, program->branches.at(0));
+      std::string regB=makeReg();
+      CompileRec(regB, program->branches.at(1));
+      sub(destReg,regA,regB);
 
     }else if(program->type=="Assign"){
+      //introduce a new variable register named program->value
+      CompileRec(program->value,program->branches.at(0));
+      copyReg(destReg,program->value);
+
 
     }else if(program->type=="Seq"){
         for(unsigned i=0; i<program->branches.size(); i++){
@@ -69,6 +96,10 @@ void Compile(
 ){
     // Create a register to hold the final result
     std::string res=makeReg();
+
+    // create 0 constant register
+    // std::string zeroReg="zero";
+    constant(zeroReg,0);
 
     // Compile the whole thing
     CompileRec(res, program);
